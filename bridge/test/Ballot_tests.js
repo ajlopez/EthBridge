e3fd674ec75d178e99f712955050de6d9d684157
ajlopez <webmaster@ajlopez.com>
2018-10-31 18:47:20 -0300

    Ballot votes for two proposal

--- bridge/test/Ballot_tests.js
+++ bridge/test/Ballot_tests.js
@@ -34,5 +34,21 @@ contract('Ballot', function (accounts) {
         assert.equal(votes[0], accounts[0]);
         assert.equal(votes[1], accounts[1]);
     });
+    
+    it('two votes for two proposals', async function () {
+        await this.ballot.voteProposal(1, accounts[0]);
+        await this.ballot.voteProposal(2, accounts[1]);
+        
+        const votes1 = await this.ballot.proposalVotes(1);
+        const votes2 = await this.ballot.proposalVotes(2);
+        
+        assert.ok(Array.isArray(votes1));
+        assert.equal(votes1.length, 1);
+        assert.equal(votes1[0], accounts[0]);
+        
+        assert.ok(Array.isArray(votes2));
+        assert.equal(votes2.length, 1);
+        assert.equal(votes2[0], accounts[1]);
+    });
 });
 
